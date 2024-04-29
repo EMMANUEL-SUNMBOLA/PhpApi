8744ffaafc590b3d60383955083256be827e5b8e
Emmanuel <adedayoemmanuel729@gmail.com>
2024-04-29 16:28:21 +0100

    API_KEY

--- receiver.php
+++ receiver.php
@@ -1,20 +1,27 @@
 <?php
+    require_once realpath(__DIR__ . "/vendor/autoload.php");
+    
+    use Dotenv\Dotenv;
+
+    $dotenv = Dotenv::createImmutable(__DIR__ );
+    $dotenv -> load();
+
+
     ob_end_flush();
     if($_SERVER["REQUEST_METHOD"] === "POST"){
-
         echo "data received \n";
-
         $json_data = file_get_contents("php://input");
         $data = json_decode($json_data, true);
 
         if(isset($data)){
             $file = fopen("post.txt", "a+");
 
-            fwrite($file, $data["fact"] . " => " . $data["author"] . "\n");
+            // fwrite($file, $data["fact"] . " => " . $data["author"] . "\n");
     
             fclose($file);
-
-            echo "data uploaded successfully \n";
+            if($key){
+                echo "data uploaded successfully \n" . $key;
+            }
         }else{
             echo "Data has issues";
         }
